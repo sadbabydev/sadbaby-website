f45d9bf663d6e03be1f42f841a2f757dc70624fc
ernemmez <emmezeren@gmail.com>
2022-05-19 15:24:04 +0300

    roadmap design update

--- src/components/Release.jsx
+++ src/components/Release.jsx
@@ -1,101 +1,332 @@
 import React from "react";
 
-
+import done from '../assets/done.svg'
+import load from '../assets/loading.png'
 
 export default function Release() {
   return (
     <div className="roadmap" >
       <h2 className="title nav-link" id="roadmap">Our Roadmap</h2>
-      <main className='phase'>
-            <h2>2021-Q2</h2>
-            <p>IDEA FORMATION</p>
-            <p>TEAM BUILDING</p>
-            <p>WEBSITE AND SOCIAL MEDIA</p>
-            <p>SADBABY WHITE PAPER V1</p>
-            <p>SADBABY.ORG V1</p>
-            <p>PROJECT R&D STUDY</p>
-            <p>TWITTER 500 FOLLOWERS</p>
-            <p>TELEGRAM 500</p>
-            <p>BSCSCAN HOLDERS 500</p>
-        </main>
-        <main className='phase'>
-            <h2>2021-Q3</h2>
-            <p>SMART CONTRACT AUDIT</p>
-            <p>CERTIK AUDIT</p>
-            <p>LATOKEN EXCHANGE</p>
-            <p>INDOEX EXCHANGE</p>
-            <p>SUPPLY DROP / TOTAL SUPPLY</p>
-            <p>TWITTER 1500 FOLLOWERS</p>
-            <p>TELEGRAM 5000</p>
-            <p>BSCSCAN HOLDERS 5000</p>
-            <p>TRUST WALLET LOGO</p>
-            <p>BSCSCAN LOGO</p>
-        </main>
-        <main className='phase'>
-            <h2>2021-Q4</h2>
-            <p>WHITE PAPER V2</p>
-            <p>SADBABY.ORG V2</p>
-            <p>TWITTER 2000 FOLLOWERS</p>
-            <p>TELEGRAM 7000</p>
-            <p>COINGECKO APPLICATION</p>
-            <p>COINMARKETCAP APPLICATION</p>
-        </main>
-        <main className='phase'>
-            <h2>2022-Q2</h2>
-            <p>BTCIX EXCHANGE</p>
-            <p>SADBABY.ORG 2022.V2</p>
-            <p>WHITE PAPER 2022.V2</p>
-            <p>NEW CONTRACT SWAP</p>
-            <p>BSCSCAN SWAP</p>
-            <p>AUDIT SWAP</p>
-            <p>CERTIK SWAP</p>
-            <p>TRUSTWALLET LOGO SWAP</p>
-            <p>COINGECKO SWAP</p>
-            <p>COINMARKETCAP SWAP</p>
-            <p>BSCSCAN ADVERTISING</p>
-            <p>COINGECKO ADVERTISING</p>
-            <p>HOLDERS 20.000+</p>
-            <p>TWITTER 15.000+</p>
-            <p>TELEGRAM 20.000+</p>
-        </main>
-        <main className='phase'>
-            <h2>2022-Q3</h2>
-            <p>PROBIT EXCHANGE</p>
-            <p>HOTBIT EXCHANGE</p>
-            <p>SURPRISE EXCHANGE</p>
-            <p>SADBABY.ORG 2022.V3</p>
-            <p>WHITE PAPER 2022.V3</p>
-            <p>VR GLASSES PRODUCTION</p>
-            <p>SDBY METAVERSE GAME</p>
-            <p>SURPRISE EXCHANGE</p>
-            <p>CRYPTO NEWS ADS</p>
-            <p>SURPRISE EXCHANGE</p>
-            <p>COINMARKETCAP TREND</p>
-            <p>COINGECKO TREND</p>
-            <p>POOCOIN TREND</p>
-            <p>BSCSCAN ADVERTISING</p>
-            <p>HOLDERS 50.000+</p>
-            <p>TWITTER 40.000+</p>
-            <p>TELEGRAM 50.000+</p>
-        </main>
-        <main className='phase'>
-            <h2>2022-Q4</h2>
-            <p>SADBABY.ORG 2022.V4</p>
-            <p>WHITE PAPER 2022.V4</p>
-            <p>MEXC EXCHANGE</p>
-            <p>CRYPTO NEWS ADS</p>
-            <p>AIRPORT ADVERTISEMENTS</p>
-            <p>GATE IO EXCHANGE</p>
-            <p>KUCOIN EXCHANGE</p>
-            <p>TV ADVERTISEMENTS</p>
-            <p>COINMARKETCAP TREND</p>
-            <p>COINGECKO TREND</p>
-            <p>POOCOIN TREND</p>
-            <p>BSCSCAN ADVERTISING</p>
-            <p>HOLDERS 100.000+</p>
-            <p>TWITTER 80.000+</p>
-            <p>TELEGRAM 100.000+</p>
-        </main>
+        <div className="roadmap-container">
+            <div className="phase">
+                <h2>2021-Q2</h2>
+                <div className="road-tasks">
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;IDEA FORMATION
+                    </p>
+                    <p className="task">
+                    <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;TEAM BUILDING
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;WEBSITE AND SOCIAL MEDIA
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;SADBABY WHITE PAPER V1
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;SADBABY.ORG V1
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;PROJECT R&D STUDY
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;TWITTER 500 FOLLOWERS
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;TELEGRAM 500
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;BSCSCAN HOLDERS 500
+                    </p>
+                </div>
+            </div>
+            <div className="phase">
+                <h2>2021-Q3</h2>
+                <div className="road-tasks">
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;SMART CONTRACT AUDIT
+                    </p>
+                    <p className="task">
+                    <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;CERTIK AUDIT
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;LATOKEN EXCHANGE
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;INDOEX EXCHANGE
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;SUPPLY DROP / TOTAL SUPPLY
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;TWITTER 1500 FOLLOWERS
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;TELEGRAM 5000
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;BSCSCAN HOLDERS 5000
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;TRUST WALLET LOGO
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;BSCSCAN LOGO
+                    </p>
+                </div>
+            </div>
+            <div className="phase">
+                <h2>2021-Q4</h2>
+                <div className="road-tasks">
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;WHITE PAPER V2
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;SADBABY.ORG V2
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;TWITTER 2000 FOLLOWERS
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;TELEGRAM 7000
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;COINGECKO APPLICATION
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;COINMARKETCAP APPLICATION
+                    </p>
+                </div>
+            </div>
+            <div className="phase">
+                <h2>2022-Q2</h2>
+                <div className="road-tasks">
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;BTCIX EXCHANGE
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;SADBABY.ORG 2022.V2
+                    </p>
+                    <p className="task">
+                        <img src={done} alt="sadbaby" className="done-icon"  /> 
+                        &nbsp;WHITE PAPER 2022.V2
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;NEW CONTRACT SWAP
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;BSCSCAN SWAP
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;AUDIT SWAP
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  />  
+                        &nbsp;CERTIK SWAP
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;TRUSTWALLET LOGO SWAP
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  />  
+                        &nbsp;COINGECKO SWAP
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;COINMARKETCAP SWAP
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;BSCSCAN ADVERTISING
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;COINGECKO ADVERTISING
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;HOLDERS 20.000+
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;TWITTER 15.000+
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  />  
+                        &nbsp;TELEGRAM 20.000+
+                    </p>
+                </div>
+            </div>
+            <div className="phase">
+                <h2>2022-Q3</h2>
+                <div className="road-tasks">
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;PROBIT EXCHANGE
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;HOTBIT EXCHANGE
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;SURPRISE EXCHANGE
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;SADBABY.ORG 2022.V3
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;WHITE PAPER 2022.V3
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;VR GLASSES PRODUCTION
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;SDBY METAVERSE GAME
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;SURPRISE EXCHANGE
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;BSCSCAN HOLDERS 500
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;SURPRISE EXCHANGE
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;COINMARKETCAP TREND
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;COINGECKO TREND
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;POOCOIN TREND
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;BSCSCAN ADVERTISING
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;HOLDERS 50.000+
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;TWITTER 40.000+
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;TELEGRAM 50.000+
+                    </p>
+                </div>
+            </div>
+            <div className="phase">
+                <h2>2022-Q4</h2>
+                <div className="road-tasks">
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;SADBABY.ORG 2022.V4
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;WHITE PAPER 2022.V4
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;MEXC EXCHANGE
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;CRYPTO NEWS ADS
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;AIRPORT ADVERTISEMENTS
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;GATE IO EXCHANGE
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;KUCOIN EXCHANGE
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;TV ADVERTISEMENTS
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;COINMARKETCAP TREND
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;COINGECKO TREND
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;POOCOIN TREND
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;BSCSCAN ADVERTISING
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;HOLDERS 100.000+
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;TWITTER 80.000+
+                    </p>
+                    <p className="task">
+                        <img src={load} alt="sadbaby" className="load-icon"  /> 
+                        &nbsp;TELEGRAM 100.000+
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
